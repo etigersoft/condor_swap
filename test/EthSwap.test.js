ddb3c002943ae1180aeb49208878a9e4371bf649
etigersoft <etigersoft@icloud.com>
2021-11-15 19:11:00 -0500

    Completing sellTokens function and testing

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -40,14 +40,6 @@ contract('EthSwap', ([deployer, investor]) => {
         })
     })
 
-    /*describe('Token deployment', async () => {
-
-    })
-
-    describe('Token deployment', async () => {
-
-    })*/
-
     describe('buyTokens()', async () => {
         let result
 
@@ -72,12 +64,51 @@ contract('EthSwap', ([deployer, investor]) => {
             ethSwapbalance = await web3.eth.getBalance(ethSwap.address)
             assert.equal(ethSwapbalance.toString(), toWei('1'))
 
+            // Check logs to ensure event was emitted with correct data
             //console.log(result.logs)
             const event = result.logs[0].args
             assert.equal(event.account, investor)
             assert.equal(event.token, token.address)
             assert.equal(event.amount.toString(), toWei('100').toString())
             assert.equal(event.rate.toString(), '100')
+        })
+
+    })
+
+    describe('sellTokens()', async () => {
+        let result
+
+        before (async () => {
+            await token.approve(ethSwap.address, toWei('100'), { from: investor})
+            result = await ethSwap.sellTokens(toWei('100'), { from: investor })
+        })
+
+        it('Allows user to instantly sell tokens to ethSwap for a fixed price', async () => {
+             // Check investor balance after purchase
+             let investorBalance = await token.balanceOf(investor)
+             assert.equal(investorBalance.toString(), toWei('0'))
+
+            // Check ethSwapbalance after purchase
+            let ethSwapbalance
+
+            // Did the ethSwap address increase theirs token inventory ?
+            ethSwapbalance = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapbalance.toString(), toWei('1000000'))
+            
+            // Did the ethSwap address reduced 1 eth in exchange of the tokens sold ?
+            ethSwapbalance = await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapbalance.toString(), toWei('0'))
+
+            // Check logs to ensure event was emitted with correct data
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), toWei('100').toString())
+            assert.equal(event.rate.toString(), '100')
+
+            // FAILURE: investor cannot sell more tokens than they have (TODO: review this later)
+            await ethSwap.sellTokens(toWei('500'), { from: investor }).should.be.rejected;
+
         })
 
     })
